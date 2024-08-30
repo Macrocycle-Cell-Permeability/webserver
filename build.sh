9a522e4195fdfe3366cddd0321f66fcb37f72965
“Xiaokonglong99” <fungcs9989@zohomail.com>
2024-08-30 16:05:18 +0200

    New

--- build.sh
+++ build.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 set -e
 
-echo "Python version:"
-python --version
 
 echo "Upgrading pip..."
 pip install --upgrade pip
 
-echo "Installing numpy separately..."
-pip install numpy==1.21.0 --no-binary :all:
+
 
 echo "Installing other dependencies..."
 pip install --no-cache-dir -r requirements.txt
