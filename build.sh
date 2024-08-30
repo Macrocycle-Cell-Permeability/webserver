52b8735c2111163a08f81488d63295a2f21a3d2a
“Xiaokonglong99” <fungcs9989@zohomail.com>
2024-08-30 15:49:29 +0200

    Add build script for Cloudflare Pages

--- build.sh
+++ build.sh
@@ -7,7 +7,10 @@ python --version
 echo "Upgrading pip..."
 pip install --upgrade pip
 
-echo "Installing dependencies..."
+echo "Installing numpy separately..."
+pip install numpy==1.21.0 --no-binary :all:
+
+echo "Installing other dependencies..."
 pip install --no-cache-dir -r requirements.txt
 
 echo "Collecting static files..."
