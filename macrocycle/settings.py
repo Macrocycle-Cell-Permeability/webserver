0e9d24911e6f0087171fc1518fc5ca66d41222bb
“Xiaokonglong99” <fungcs9989@zohomail.com>
2024-08-30 18:41:15 +0200

    Allow

--- macrocycle/settings.py
+++ macrocycle/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-#^2bf2g!nf2bkh!^o7#p^9nr-0&1b$!hzfgying%g*x2qb!_^u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','16.171.134.173' ]
+ALLOWED_HOSTS = ['127.0.0.1','16.171.134.173','swemacrocycledb.com' ,'www.swemacrocycledb.com']
 
 # Application definition
 
